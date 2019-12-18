34df8de6640ebff1e4eb3aa5797d31cb8d27046f
Christian Lumapac <lumapac.christian.paculanang-ext@continental-corporation.com>
2019-12-18 16:32:23 +0800

    Configured layouts
    * album list
    * album item
    * ...and styling

--- app/src/main/java/com/example/rxandroidmvvm/view/ListFragment.kt
+++ app/src/main/java/com/example/rxandroidmvvm/view/ListFragment.kt
@@ -16,5 +16,7 @@ class ListFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_list, container, false)
     }
 
-
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
 }
